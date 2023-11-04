d85eda5b6fc76a7183eacaaebb53bfdded7f817f
JohannesGlier <45363793+JohannesGlier@users.noreply.github.com>
2023-11-04 12:33:50 +0100

    Test

---
+++ Assets/Scripts/GetUserName.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GetUserName : MonoBehaviour
+{
+    private string username;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        username = System.Environment.UserName;
+        this.GetComponent<TMP_Text>().text = username;
+    }
+}
